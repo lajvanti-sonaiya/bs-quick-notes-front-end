1b2074ee4c4bc535447780e12da0a1e806bef6d7
bs-lajvanti-sonaiya <lajvanti.sonaiya@bombaysoftwares.com>
2026-03-02 17:11:38 +0530

    added not found page

--- src/app/_not-found.tsx
+++ src/app/_not-found.tsx
@@ -1,1 +1,8 @@
 export const dynamic = "force-dynamic"; // prevents static prerender
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
+    </div>
+  );
+}
